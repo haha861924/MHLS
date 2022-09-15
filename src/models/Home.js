9dc6eea4513e8fd72182277152ead949d108d2d6
haha861924 <qq861924@gmail.com>
2022-09-15 09:48:49 +0800

    feat: 🎸 home model

---
+++ src/models/Home.js
@@ -0,0 +1,5 @@
+export const List = [
+  { title: "儲存資料", describe: "網站將會儲存資料在您的設備" },
+  { title: "快速分析", describe: "視覺化的圖表，讓您可以直接看圖說故事" },
+  { title: "持續追蹤", describe: "固定使用所累積的資料，有利於您追蹤資料" },
+];
