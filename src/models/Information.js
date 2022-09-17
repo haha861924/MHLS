6b8c4c64655e1b1259d2a0347f1251152ed20bb8
haha861924 <qq861924@gmail.com>
2022-09-17 18:38:58 +0800

    feat: 🎸 add constance

---
+++ src/models/Information.js
@@ -0,0 +1,22 @@
+export const Gender = [
+  { describe: "男性", value: 1 },
+  { describe: "女性", value: 2 },
+];
+
+export const Activity = [
+  { describe: "幾乎不動" },
+  { describe: "輕度" },
+  { describe: "中度" },
+  { describe: "重度" },
+  { describe: "極重度" },
+];
+
+export const Pregnancy = [
+  { describe: "" },
+  { describe: "第一孕期" },
+  { describe: "第二孕期" },
+  { describe: "第三孕期" },
+  { describe: "哺乳" },
+];
+
+export const Basic = [{ data: 173, id: "身高(公分)" }];
