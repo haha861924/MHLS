bb3c86dababe26017c354d82c00c70e373bdd511
haha861924 <qq861924@gmail.com>
2022-09-18 20:56:14 +0800

    feat: 🎸 add svg icon

--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -1,11 +1,13 @@
 import { ReactComponent as logo } from "./logo.svg";
 import { ReactComponent as item } from "./item.svg";
 import { ReactComponent as fork } from "./fork.svg";
+import { ReactComponent as info } from "./information.svg";
 import { ReactComponent as background } from "./background.svg";
 
 export const Icon = {
   logo,
   item,
   fork,
+  info,
   background,
 };
