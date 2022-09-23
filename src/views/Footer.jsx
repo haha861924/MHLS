789f90ef303ed33ef468a36812b7ade65fc9dae8
haha861924 <qq861924@gmail.com>
2022-09-23 23:30:43 +0800

    style: 💄 icon introduction

--- src/views/Footer.jsx
+++ src/views/Footer.jsx
@@ -5,14 +5,16 @@ export function Footer() {
   return (
     <div className="flex w-fit text-blue-gray mt-10 font-black">
       {List.map((item) => (
-        <div className="text-blue-gray flex flex-col" key={item.title}>
-          <div className="flex">
-            <Icon.item />
-            <label className="text-white bg-blue-gray">{item.title}</label>
+        <div className="mr-3 relative w-56">
+          <div className="flex items-center">
+            <div className=" bg-white z-10 flex h-12 w-12 items-center justify-center rounded-full border-4 p-2 border-blue-gray">
+              <Icon.item />
+            </div>
+            <label className="absolute  ml-10 bg-blue-gray text-white rounded-r p-1 px-3">
+              {item.title}
+            </label>
           </div>
-          <div>
-            <p>{item.info}</p>
-          </div>
+          <p className="ml-5 mt-5">{item.info}</p>
         </div>
       ))}
     </div>
